248d9a440832a9ef4de3917b2ce61a03c78316bb
Jonathan Burggraf <jonathan.burggraf@gmail.com>
2016-04-24 11:24:12 +0200

    Add package.json and Procfile

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 var express = require('express');
 var app = express();
-var port = 3000;
+var port = process.env.PORT || 3000;
 var server = app.listen(port);
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 var redis = require('redis');
-var client = redis.createClient(6379);
+var client = redis.createClient(process.env.PORT || 6379);
 
 app.get('/update:lastTick', function updater(req, res) {
   client.llen('ticks', function (err,reply) {
     if (err) {
       console.log(err);
     }
-    if(req.params.lastTick == reply){
+    if(req.params.lastTick >= reply){
       res.sendStatus(204);
       res.end();
     }
@@ -33,7 +33,6 @@ app.get('/update:lastTick', function updater(req, res) {
 
 app.post('/tick', function newTickAdder(req, res) {
   client.rpush('ticks', req.body.post);
-  console.log(client.lrange('ticks', 0, -1));
   res.sendStatus(201);
   res.end();
 })
